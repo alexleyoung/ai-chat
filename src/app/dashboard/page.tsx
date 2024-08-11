b72815b6caa06ee2350e2012f96e2d9b4903a19a
alankang30 <alankang30@gmail.com>
2024-08-11 10:28:38 -0500

    added dashboard button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Dashboard() {
   return (
-    <section className='ml-96 grid place-items-center h-screen w-full'></section>
+    <section className='ml-96 grid place-items-center h-screen w-full'>
+      <Button>
+        hi
+      </Button>
+    </section>
   );
 }
